658b03a9a7f473a0656e2a871c63d47a5a729257
Wmeyreles <40470543+Wmeyreles@users.noreply.github.com>
2018-08-15 11:06:51 -0300

    subida del proyecto

---
+++ Nueva carpeta/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajo_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c1, c2, c3, c4, c5, tc, p;
+            Console.Write("primera calificacion");
+            c1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("segunda calificacion");
+            c2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("tercera calificacion");
+            c3 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("cuarta calificacion");
+            c4 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("quinta calificacion");
+            c5 = Convert.ToInt32(Console.ReadLine());
+            tc = c1 + c2 + c3 + c4 + c5;
+            p = tc / 5;
+            Console.WriteLine("el total de las calificaciones es {0}", tc);
+            Console.WriteLine("el promedio de las calificaciones es {0}", p);
+            Console.ReadKey();
+        }
+    }
+}
